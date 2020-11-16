78e9b9f47cceecec87870d8a98ba1a0b9b36372d
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2020-11-16 11:01:14 +0100

    deployment ready

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO EVERYBODY, THIS IS MY FIRST WEBSITE USING REACT!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO WORLD!
         </a>
       </header>
     </div>
