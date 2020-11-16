35adb1f1c96bc085d84ba61516aa8a9c0eb53955
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2020-11-16 12:22:55 +0100

    again, new header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HELLO , THIS IS MY FIRST WEBSITE USING REACT!</p>
+        <p>THIS IS MY FIRST WEBSITE USING REACT!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
