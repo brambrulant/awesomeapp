f35c70520d231135aff123d5fc0a26c7b91cae1a
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2020-11-16 12:17:39 +0100

    new header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO EVERYBODY, THIS IS MY FIRST WEBSITE USING REACT!
-        </p>
+        <p>HELLO , THIS IS MY FIRST WEBSITE USING REACT!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
